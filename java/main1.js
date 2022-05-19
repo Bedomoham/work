772a86bc78bf08b3fd6be8f49ef49bb41ed7ebf5
Bedomoham <105437934+Bedomoham@users.noreply.github.com>
2022-05-20 01:23:31 +0200

    Add files via upload

---
+++ java/main1.js
@@ -0,0 +1,9 @@
+function test()
+{
+    document.getElementById("random").innerHTML="the best revenge is massive success ";
+    
+}
+function test2()
+{
+    document.getElementById("random").innerHTML="hello";
+}
